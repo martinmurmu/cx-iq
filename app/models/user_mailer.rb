5acd831d6c32c8a2dd4f4aef2fe99708db91858e
Vitaly Zemlyansky <vitozemlya@gmail.com>
2014-12-08 15:05:38 +0600

    update support email

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base 
   def product_report(user, report, output_format)
     recipients  user.email
-    from        "support@amplifiedanalytics.com"
+    from        "support@cx-iq.com"
     subject     "Report"
     body        :user => user
     
@@ -20,7 +20,7 @@ class UserMailer < ActionMailer::Base
 
   def psa_report_completion(user, url)
     recipients  user.email
-    from        "support@amplifiedanalytics.com"
+    from        "support@cx-iq.com"
     subject     "PSA Report"
     body        :url => url
 
@@ -28,35 +28,35 @@ class UserMailer < ActionMailer::Base
 
   def cia_report(user, url,host,product_group_id)
     recipients  user.email
-    from        "support@amplifiedanalytics.com"
+    from        "support@cx-iq.com"
     subject     "Market Report"
     body        :url => url, :host => host, :product_group_id=>product_group_id
   end
 
   def trending_report(user, url)
     recipients  user.email
-    from        "support@amplifiedanalytics.com"
+    from        "support@cx-iq.com"
     subject     "Trending Report"
     body        :url => url
   end
 
   def trending_products_report(user, url)
     recipients  user.email
-    from        "support@amplifiedanalytics.com"
+    from        "support@cx-iq.com"
     subject     "Trending Product Report"
     body        :url => url
   end
   
   def paper_download(paper_download)
     recipients  paper_download.email
-    from        "support@amplifiedanalytics.com"
+    from        "support@cx-iq.com"
     subject     "White Paper Download"
     body        :paper_download => paper_download
   end
   
   def paper_download_info(paper_download)
     recipients  "greg@amplifiedanalytics.com"
-    from        "support@amplifiedanalytics.com"
+    from        "support@cx-iq.com"
     subject     "White Paper Download By A User"
     body        :paper_download => paper_download
   end
